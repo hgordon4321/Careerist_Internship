92670c781f8515a133399b7f6b07c63d357bcc44
hgordon4321 <hgordon4321@gmail.com>
2023-05-01 09:26:01 -0500

    Change browser to firefox (headless)

--- features/environment.py
+++ features/environment.py
@@ -6,10 +6,16 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    context.driver = webdriver.Chrome(chrome_options=options)
-    # context.browser = webdriver.Firefox()
+    # Chrome Setup
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # context.driver = webdriver.Chrome(chrome_options=options)
+
+    # Firefox setup
+    options = webdriver.FirefoxOptions()
+    options.binary_location = "C:/Program Files/Mozilla Firefox/firefox.exe"
+    options.add_argument('--headless')
+    context.driver = webdriver.Firefox(options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
